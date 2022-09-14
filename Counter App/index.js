1b3f2210a6dfbb9b8e8b67effde84eea32bced19
Nahiyan-16 <71025942+Nahiyan-16@users.noreply.github.com>
2022-09-14 11:15:21 -0400

    Add files via upload

---
+++ Counter App/index.js
@@ -0,0 +1,36 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let keepEl = document.getElementById("keep-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+let keepCount = 0;
+
+function save() {
+    if(keepCount == 0){
+        saveEl.textContent += count
+        countEl.textContent = 0
+        count = 0
+        keepCount += 1;
+    }
+    else if(keepCount < 10 && keepCount != 0){
+        saveEl.textContent += " - " + count
+        countEl.textContent = 0
+        count = 0
+        keepCount += 1;
+    }
+    else{
+        keepEl.textContent = "NO MORE SPACE (MUST RESET)"
+    }
+}
+
+function reset() {
+    saveEl.textContent = "Previous entries: ";
+    count = 0;
+    countEl.textContent = 0;
+    keepEl.textContent = "";
+}

120135dd06e5f91a0870b5504f32882fed037159
Nahiyan-16 <71025942+Nahiyan-16@users.noreply.github.com>
2022-10-05 22:45:15 -0400

    1

--- MemePicker/index.js
+++ MemePicker/index.js
@@ -7,6 +7,10 @@ const memeModalInner = document.getElementById('meme-modal-inner')
 const memeModal = document.getElementById('meme-modal')
 const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
 
+function closeFromBackground(){
+    memeModal.style.display = 'none'
+}
+
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
 memeModalCloseBtn.addEventListener('click', closeModal)
@@ -99,7 +103,3 @@ function renderEmotionsRadios(cats){
 }
 
 renderEmotionsRadios(catsData)
-
-
-
-

051788f8752311c09c163efbf486c14e8ba3a9dc
Nahiyan-16 <71025942+Nahiyan-16@users.noreply.github.com>
2022-11-05 20:27:29 -0400

    added js functionality

--- Tindog/JS/Dog.js
+++ Tindog/JS/Dog.js
@@ -2,4 +2,11 @@ export class Dog{
     constructor(data){
         Object.assign(this, data)
     }
+    isNotLiked(){
+        this.hasBeenSwiped = true
+    }
+    isLiked(){
+        this.hasBeenSwiped = true
+        this.hasBeenLiked = true
+    }
 }

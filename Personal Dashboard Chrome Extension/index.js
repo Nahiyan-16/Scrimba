0da0ea1221c6e24ec8973f4d30d794c9fb56953f
Nahiyan-16 <71025942+Nahiyan-16@users.noreply.github.com>
2022-11-17 22:15:36 -0500

    personal dashboard added

--- Personal Dashboard Chrome Extension/index.js
+++ Personal Dashboard Chrome Extension/index.js
@@ -5,37 +5,10 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
 		document.getElementById("author").textContent = `By: ${data.user.name}`
     })
     .catch(err => {
-        // Use a default background image/author
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
 )`
 		document.getElementById("author").textContent = `By: Dodi Achmad`
     })
-
-// fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-//     .then(res => {
-//         if (!res.ok) {
-//             throw Error("Something went wrong")
-//         }
-//         return res.json()
-//     })
-//     .then(data => {
-//         document.getElementById("crypto-top").innerHTML = `
-//             <img src=${data.image.small} />
-//             <span>${data.name}</span>
-//         `
-//         document.getElementById("crypto").innerHTML += `
-//             <p>🎯: $${data.market_data.current_price.usd}</p>
-//             <p>👆: $${data.market_data.high_24h.usd}</p>
-//             <p>👇: $${data.market_data.low_24h.usd}</p>
-//         `
-//     })
-//     .catch(err => console.error(err))
-
-document.getElementById('myLinks').innerHTML = `
-    <div class="links">
-        <p><a href='https://www.youtube.com/'><img src='YTicon.png'></a></p>
-    </div>
-`
 
 function getCurrentTime() {
     const date = new Date()
@@ -62,3 +35,121 @@ navigator.geolocation.getCurrentPosition(position => {
         })
         .catch(err => console.error(err))
 });
+
+const addLinkContainer = document.getElementsByClassName('addLink')[0]
+const addLinkBtn = document.getElementById('addLinkBtn')
+const addFormBtn = document.getElementById('addFormBtn')
+const allLinksContainer = document.getElementById('allLinks')
+const removeLinkBtn = document.getElementById('removeLinkBtn')
+const removeLinkEl = document.getElementsByClassName('removeLink')
+
+if(!localStorage.getItem('linksCapacity')){
+    localStorage.setItem('linksCapacity', 0)
+}
+let capacity = localStorage.getItem('linksCapacity')
+
+const toggleAddContainer = ()=>{
+    addLinkContainer.classList.toggle('displayNone')
+}
+
+toggleAddContainer()
+
+addLinkBtn.addEventListener('click', toggleAddContainer)
+
+
+addFormBtn.addEventListener('click', ()=>{
+    let Title = document.getElementById('formTitle')
+    let URL = document.getElementById('formURL')
+
+    if(Title.value && URL.value && capacity < 18){
+        capacity++
+        localStorage.setItem('linksCapacity', capacity)
+        let links = [[Title.value, URL.value]]
+        if(localStorage.myLinks){
+            const storedLinks = JSON.parse(localStorage.myLinks)
+            storedLinks.push([Title.value, URL.value])
+            localStorage.myLinks = JSON.stringify(storedLinks)
+        }
+        else{
+            localStorage.myLinks = JSON.stringify(links)
+        }
+        renderLinks()
+    } 
+    Title.value = ''
+    URL.value = ''
+    toggleAddContainer()
+})
+
+function renderLinks(){
+    if(localStorage.myLinks){
+        const storedLinks = JSON.parse(localStorage.myLinks)
+        let html = ''
+        storedLinks.forEach(e => {
+            html += `
+                <div class="links">
+                    <button type='button' class="removeLink displayNone">−</button>
+                    <a href='https://${e[1]}' target='_blank'>${e[0]}</a>
+                </div>
+            `
+        })
+        allLinksContainer.innerHTML = html
+    }
+}
+
+document.body.addEventListener('click', e=>{
+    if(e.target)if(e.target.nextSibling)if(e.target.nextSibling.nextSibling)if(e.target.nextSibling.nextSibling.firstChild){
+        deleteLink(e.target.nextSibling.nextSibling.firstChild.textContent)
+    }
+    
+})
+
+function deleteLink(e){
+    if(localStorage.getItem('linksCapacity')){
+        let storedLinks = JSON.parse(localStorage.myLinks)
+        let ary = []
+        storedLinks.forEach(element=>{
+            if(element[0] !== e){
+                ary.push(element)
+            }
+            else{
+                capacity--
+                localStorage.setItem('linksCapacity', capacity)
+            }
+        })
+        localStorage.myLinks = JSON.stringify(ary)
+        renderLinks()
+    }
+}
+
+removeLinkBtn.addEventListener('click', ()=>{
+    document.querySelectorAll(".removeLink").forEach(e=>{
+        e.classList.toggle('displayNone')
+    })
+})
+
+document.getElementById('getShowName').addEventListener('keypress', e => {
+    if(e.key === 'Enter'){
+        localStorage.setItem('name', document.getElementById('getShowName').value)
+        renderName()
+    }
+})
+
+function renderName(){
+    if(localStorage.getItem('name')){
+        let name = localStorage.getItem('name')
+        fetch('https://www.greetingsapi.com/random')
+        .then(res=>res.json())
+        .then(data=>{
+            document.getElementById('showName').innerHTML = `
+            <h2>${data.greeting}, ${name}</h2>
+            <p>(${data.language})</p>
+            `
+        })
+
+    }
+}
+
+renderName()
+renderLinks()
+
+

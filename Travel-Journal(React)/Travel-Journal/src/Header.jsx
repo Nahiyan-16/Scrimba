df3c96f41763a29630ebfb4fbebc117c3f2532c1
Nahiyan-16 <71025942+Nahiyan-16@users.noreply.github.com>
2022-11-30 19:28:17 -0500

    1.1

---
+++ Travel-Journal(React)/Travel-Journal/src/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header(){
+    return(
+        <div className='Header-Container'>
+            <p>🌎 my Travel Journal</p>
+        </div>
+    )
+}
